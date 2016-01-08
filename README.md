7482a99d7256a1c6313adc6b3aca0f53c7d7bf25
Joe Hillenbrand <joehillen@gmail.com>
2016-01-07 16:59:42 -0800

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+# ultron
+
+A Slack Bot for UNIX-style ChatOps
+
+# Usage
+
+    $ ultron --help
+    ultron - a Unix-style ChatOps bot for Slack
+
+    Usage: ultron CONFIG
+
+    Available options:
+      -h,--help                Show this help text
+
+
+## Configure
+
+See [](example.cfg)
+
+## Built-In Commands
+
+* `help` -- list available commands
+
+## Writing Commands
+
+Commands are just programs (The UNIX Philosophy)
+
+If the exit code of the command program is zero,
+STDOUT is written directly to slack unformatted
+with `@username` for the person who called the command.
+Formatting is up to the command program. See [Slack Message Formatting](https://api.slack.com/docs/formatting)
+
+If the exit code of the command program is non-zero,
+the command is treated as a failure
+and the STDOUT and STDERR printed if they had any output.
+
+Two environment variables are passed to command program:
+
+* `ULTRON_UID` -- the user id that called the command
+* `ULTRON_CID` -- the channel id were the command was written
+
+
+# Build
+
+```
+stack build
+```
+
+# Known Issues
+
+* Responses are logged to stdout.
+* Some message types are not handled properly: https://github.com/mpickering/slack-api/issues/37
+* Only one process per channel can run at a time.
+* If a process runs forever, ultron must be restarted.
+
+# Wishlist (PRs Welcome)
+
+* Quoted Strings (with escaping)
+* UNIX pipes
+* Process Control (list running commands and kill them)
+* Attachments Support
---
+++ app/Main.hs
@@ -0,0 +1,148 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Concurrent.Async
+import Control.Exception (handle, SomeException)
+import Control.Lens hiding (argument)
+import Control.Monad
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Ini as INI
+import qualified Data.Ini.Reader as INI
+import Data.List (nub)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Options.Applicative
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Process
+import System.Process.Text as PT
+import Web.Slack
+import Web.Slack.Message
+
+
+main :: IO ()
+main = do
+  cfgfp <- execParser $
+    info (helper <*> argument str (metavar "CONFIG"))
+         (  fullDesc
+         <> header "ultron - A Slack Bot for UNIX-style ChatOps"
+         )
+  cfgs <- readcfg cfgfp <$> readFile cfgfp
+  void $ mapConcurrently runUltron cfgs
+
+
+data UltronCfg
+  = UltronCfg { prefix  :: Text
+              , channel :: Text
+              , token   :: String
+              , paths   :: [FilePath]
+              }
+
+
+runUltron :: UltronCfg -> IO ()
+runUltron cfg = runBot (SlackConfig (token cfg)) (ultron cfg) ()
+
+
+ultron :: UltronCfg -> SlackBot ()
+ultron cfg (Message cid (UserComment uid) msg _ _ _) =
+  unless (cid ^. getId /= channel cfg) $
+    case parsemsg (prefix cfg) msg of
+      Nothing -> return ()
+      Just (cmd, args) -> do
+        resp <- liftIO $ runcmd cid uid (paths cfg) cmd args
+        sendMessage cid ("<@"<>uid ^. getId<>">\n"<>resp)
+ultron _ _ = return ()
+
+
+readcfg :: FilePath -> String -> [UltronCfg]
+readcfg fp s =
+  case INI.parse s of
+    Left  err  -> error $ show err
+    Right cfg  -> map (go cfg) $ INI.sections cfg
+ where
+  go cfg name =
+    case INI.getSection name cfg of
+      Nothing -> error $ "Section `" ++ name ++ "` not found in " ++ fp
+      Just _ ->
+        let getOpt optname =
+              fromMaybe
+                (error $ optname ++ " not found in section `" ++ name ++ "` in " ++ fp)
+                (INI.getOption name optname cfg) in
+        UltronCfg
+        { prefix  = T.pack $ getOpt "prefix"
+        , channel = T.pack $ getOpt "channel"
+        , token   = getOpt "token"
+        , paths   = map T.unpack . T.splitOn ":" . T.pack $ getOpt "paths"
+        }
+
+
+parsemsg :: Text -> Text -> Maybe (Text, [Text])
+parsemsg pref msg =
+  let preflen = T.length pref in
+  if T.null msg || T.length msg < preflen || T.take preflen msg /= pref
+    then Nothing
+    else case T.words $ T.drop preflen msg of
+           [] -> Nothing
+           xs -> Just (head xs, tail xs)
+
+
+runcmd :: ChannelId -> UserId -> [FilePath] -> Text -> [Text] -> IO Text
+runcmd cid uid paths' cmd args =
+  handle
+    (\e -> return $ "*ERROR:* "<>tshow (e :: SomeException))
+    (if cmd == "help" then do
+       bins <- listBins paths'
+       return $ T.intercalate "\n" (map T.pack bins)
+     else do
+       binMB <- getBin paths' cmd
+       case binMB of
+         Nothing -> return $ "*ERROR:* Unknown command: "<>cmd
+         Just bin -> runbin bin)
+ where
+  runbin bin = do
+    let p = (proc bin (map T.unpack args))
+              { env = Just [ ("ULTRON_CID", T.unpack (cid ^. getId))
+                           , ("ULTRON_UID", T.unpack (uid ^. getId))
+                           ]
+              , close_fds = True
+              }
+    (ec, stdout, stderr) <- PT.readCreateProcessWithExitCode p ""
+    return $ case ec of
+                ExitSuccess -> stdout
+                ExitFailure x ->
+                  "*ERROR:* `"<>cmd<>"` failed with exit code: "<>tshow x<>"\n"
+                  <> if not $ T.null stdout
+                       then "*stdout:*\n```\n"<>stdout<>"```\n"
+                       else ""
+                  <> if not $ T.null stderr
+                       then "*stderr:*\n```\n"<>stderr<>"```"
+                       else ""
+
+
+listBins :: [FilePath] -> IO [FilePath]
+listBins paths' = do
+  existing <- filterM doesDirectoryExist paths'
+  allbins <- concat <$> mapM (\p -> filterM (isExec p) =<< getDirectoryContents p) existing
+  return $ nub allbins
+
+getBin :: [FilePath] -> Text -> IO (Maybe FilePath)
+getBin paths' cmd = do
+  filteredPaths <- filterM filterPath =<< filterM doesDirectoryExist paths'
+  case filteredPaths of
+    [] -> return Nothing
+    path:_ -> return . Just $ path </> cmdstr
+ where
+  cmdstr = T.unpack cmd
+  filterPath path = do
+    files <- filterM (isExec path) =<< getDirectoryContents path
+    return $ cmdstr `elem` files
+
+
+isExec :: FilePath -> FilePath -> IO Bool
+isExec path name = executable <$> getPermissions (path </> name)
+
+
+tshow :: Show a => a -> Text
+tshow = T.pack . show
