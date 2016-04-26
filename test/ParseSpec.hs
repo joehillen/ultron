e2c7ea767e051f7b1017b23eb048e90ac3f6de8a
Joe Hillenbrand <joehillen@gmail.com>
2016-04-26 14:24:24 -0700

    use hspec-megaparsec

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -3,8 +3,8 @@
 {-# LANGUAGE QuasiQuotes #-}
 module ParseSpec (spec) where
 
-import Data.Either (isLeft)
 import Test.Hspec
+import Test.Hspec.Megaparsec
 import Text.Heredoc
 -- import Test.Hspec.QuickCheck
 
@@ -14,66 +14,66 @@ spec :: Spec
 spec =
   describe "parseCommand" $ do
     it "returns Left given empty string" $
-      parseCommand "" `shouldSatisfy` isLeft
+      parseCommand `shouldFailOn` ""
     it "returns Left given space" $
-      parseCommand " " `shouldSatisfy` isLeft
+      parseCommand `shouldFailOn` " "
     it "handles a single command" $
-      parseCommand "a" `shouldBe` Right ("a", [])
+      parseCommand "a" `shouldParse` ("a", [])
     it "handles a single command with trailing whitespace" $
-      parseCommand "a         " `shouldBe` Right ("a", [])
+      parseCommand "a         " `shouldParse` ("a", [])
     it "handles one arg" $
-      parseCommand "cmd b" `shouldBe` Right ("cmd", ["b"])
+      parseCommand "cmd b" `shouldParse` ("cmd", ["b"])
     it "handles two arg" $
-      parseCommand "a b c" `shouldBe` Right ("a", ["b", "c"])
+      parseCommand "a b c" `shouldParse` ("a", ["b", "c"])
     it "handles extra space" $
-      parseCommand "a    b" `shouldBe` Right ("a", ["b"])
+      parseCommand "a    b" `shouldParse`  ("a", ["b"])
     it "handles leading and trailing whitespace" $
-      parseCommand "    abc    b    c   " `shouldBe` Right ("abc", ["b", "c"])
+      parseCommand "    abc    b    c   " `shouldParse`  ("abc", ["b", "c"])
     it "accepts empty double quotes" $
-      parseCommand [here|cmd ""|] `shouldBe` Right ("cmd", [""])
+      parseCommand [here|cmd ""|] `shouldParse`  ("cmd", [""])
     it "accepts double quoted whitespace" $
-      parseCommand [here|cmd "  "|] `shouldBe` Right ("cmd", ["  "])
+      parseCommand [here|cmd "  "|] `shouldParse`  ("cmd", ["  "])
     it "accepts double quoted strings" $
-      parseCommand [here|cmd "foo bar"|] `shouldBe` Right ("cmd", ["foo bar"])
+      parseCommand [here|cmd "foo bar"|] `shouldParse`  ("cmd", ["foo bar"])
     it "accepts double quoted double quotes" $
-      parseCommand [here|cmd " \" "|] `shouldBe` Right ("cmd", [[here| " |]])
+      parseCommand [here|cmd " \" "|] `shouldParse`  ("cmd", [[here| " |]])
     it "accepts literal backslash" $
-      parseCommand [here|cmd \|] `shouldBe` Right ("cmd", ["\\"])
+      parseCommand [here|cmd \|] `shouldParse`  ("cmd", ["\\"])
     it "accepts escaped backslash in double quotes" $
-      parseCommand [here|cmd "\\"|] `shouldBe` Right ("cmd", ["\\"])
+      parseCommand [here|cmd "\\"|] `shouldParse`  ("cmd", ["\\"])
     it "accepts escaped backslash in single quotes" $
-      parseCommand [here|cmd '\\'|] `shouldBe` Right ("cmd", ["\\"])
+      parseCommand [here|cmd '\\'|] `shouldParse`  ("cmd", ["\\"])
     it "accepts double quoted single quotes" $
-      parseCommand [here|cmd "'" "'"|] `shouldBe` Right ("cmd", ["'", "'"])
+      parseCommand [here|cmd "'" "'"|] `shouldParse`  ("cmd", ["'", "'"])
     it "accepts single quotes" $
-      parseCommand [here|cmd ' foo bar '|] `shouldBe` Right ("cmd", [" foo bar "])
+      parseCommand [here|cmd ' foo bar '|] `shouldParse`  ("cmd", [" foo bar "])
     it "accepts single quoted double quotes" $
-      parseCommand [here|cmd '""'|] `shouldBe` Right ("cmd", [[here|""|]])
+      parseCommand [here|cmd '""'|] `shouldParse`  ("cmd", [[here|""|]])
     it "accepts single quoted single quotes" $
-      parseCommand [here|cmd '\' \''|] `shouldBe` Right ("cmd", ["' '"])
+      parseCommand [here|cmd '\' \''|] `shouldParse`  ("cmd", ["' '"])
     it "accepts single quoted double quotes around words" $
-      parseCommand [here|cmd '" foo bar "'|] `shouldBe` Right ("cmd", [[here|" foo bar "|]])
+      parseCommand [here|cmd '" foo bar "'|] `shouldParse`  ("cmd", [[here|" foo bar "|]])
     it "accepts single quoted double quotes around escaped single quotes" $
       parseCommand [here|cmd '" \'foo\' "' '" \'bar\' "'|]
-        `shouldBe` Right ("cmd", [[here|" 'foo' "|], [here|" 'bar' "|]])
+        `shouldParse`  ("cmd", [[here|" 'foo' "|], [here|" 'bar' "|]])
     it "accepts doulbe quoted single quotes around escaped double quotes" $
       parseCommand [here|cmd "' \"foo bar\" '"|]
-        `shouldBe` Right ("cmd", [[here|' "foo bar" '|]])
+        `shouldParse`  ("cmd", [[here|' "foo bar" '|]])
     it "accepts single quoted double quotes around escaped single quotes then around escaped double quotes" $
       parseCommand [here|cmd '" \'foo \"bar\"\' "'|]
-        `shouldBe` Right ("cmd", [[here|" 'foo \"bar\"' "|]])
+        `shouldParse`  ("cmd", [[here|" 'foo \"bar\"' "|]])
     it "accepts double quoted string ending in an escaped backslash" $
       parseCommand [here|cmd " foo bar\\"|]
-        `shouldBe` Right ("cmd", [[here| foo bar\|]])
+        `shouldParse`  ("cmd", [[here| foo bar\|]])
     it "accepts single quoted string ending in an escaped backslash" $
       parseCommand [here|cmd ' foo bar\\'|]
-        `shouldBe` Right ("cmd", [[here| foo bar\|]])
+        `shouldParse`  ("cmd", [[here| foo bar\|]])
     it "accepts a single quoted escaped backslash and double quote" $
       parseCommand [here|cmd '\\"'|]
-        `shouldBe` Right ("cmd", [[here|\"|]])
+        `shouldParse`  ("cmd", [[here|\"|]])
     it "accepts a double quoted escaped backslash and escaped double quote" $
       parseCommand [here|cmd "\\\""|]
-        `shouldBe` Right ("cmd", [[here|\"|]])
+        `shouldParse`  ("cmd", [[here|\"|]])
     it "does not interpret other escaped characters" $
       parseCommand [here|cmd "hello\nworld"|]
-        `shouldBe` Right ("cmd", [[here|hello\nworld|]])
+        `shouldParse`  ("cmd", [[here|hello\nworld|]])
